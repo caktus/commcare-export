f2074b98621495159badce7a73b644d8ccc913fd
Benjamin White <960351+benjaminEwhite@users.noreply.github.com>
2020-08-24 11:21:13 -0400

    temp script to clean db

--- clean-db.sql
+++ clean-db.sql
@@ -1,6 +1,10 @@
-select 'drop table if exists "' || tablename || '" cascade;'
-from pg_tables
-where schemaname = 'public';
-select 'drop table if exists "' || tablename || '" cascade;'
-from pg_tables
-where schemaname = 'test';
+DO $$
+DECLARE r RECORD;
+BEGIN FOR r IN (
+	SELECT tablename,
+		schemaname
+	FROM pg_tables
+	WHERE schemaname IN ('public', 'test')
+) LOOP EXECUTE 'DROP TABLE ' || quote_ident(r.schemaname) || '.' || quote_ident(r.tablename) || ' CASCADE';
+END LOOP;
+END $$;
